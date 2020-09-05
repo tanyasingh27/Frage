4315541c1685fa522535e8f3b1b2bb4869d328ff
Tanya Singh <tanyacs27@gmail.com>
2020-09-06 00:09:34 +0530

    Updates

---
+++ precache-manifest.3ba84707c1b94a86fd2ec30ffd68315e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69c2d66be1fcdb12996e4ea67229d51",
+    "url": "/Frage/index.html"
+  },
+  {
+    "revision": "f60b2a0c664357ef2f55",
+    "url": "/Frage/static/css/main.3f3fd4a5.chunk.css"
+  },
+  {
+    "revision": "c90772567e9abfe9d530",
+    "url": "/Frage/static/js/2.b8fbce4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frage/static/js/2.b8fbce4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60b2a0c664357ef2f55",
+    "url": "/Frage/static/js/main.e04a1470.chunk.js"
+  },
+  {
+    "revision": "0410040314233c61a646",
+    "url": "/Frage/static/js/runtime-main.98fbca01.js"
+  }
+]);
